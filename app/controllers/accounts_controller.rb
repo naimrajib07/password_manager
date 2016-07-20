483f5586a544d38889e4365c639d8faa21e9670e
Naim Rajiv <naim.cse07@gmail.com>
2016-07-20 19:42:32 +0200

    Category name show instead of id in account show

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -65,7 +65,7 @@ class AccountsController < ApplicationController
   private
   # Use callbacks to share common setup or constraints between actions.
   def set_account
-    @account = Account.find(params[:id])
+    @account = Account.includes(:category).find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
